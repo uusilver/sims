30cf6d10e65603dd643bb77b2ff7dc59c8995a72
vani_junying@hotmail.com <vali@SSC1-W257.schenker-asia.com>
2014-10-22 21:32:52 +0800

    fixed pop window

--- Framework/mss/html/js/server-status.js
+++ Framework/mss/html/js/server-status.js
@@ -11,45 +11,46 @@ $(function() {
     //***************************end of loading server list info*******************************************************************
 
 
-    //Add dialog show server msg
-    $( "#dialog" ).dialog({
-        autoOpen: false,
-        width: 1100,
-        buttons: [
-            {
-                text: "关闭",
-                click: function() {
-                    $( this ).dialog( "close" );
-                }
-            }
-        ]
-    });
 
-    // Link to open the dialog
-    $( "#dialog-link" ).click(function( event ) {
-        $( "#dialog" ).dialog( "open" );
-        event.preventDefault();
-    });
 
     //dialog shows server status info
     $( "#pop" ).dialog({
         autoOpen: false,
         width: 1100,
-        buttons: [
+        height:600
+
+        /*buttons: [
             {
                 text: "关闭",
                 click: function() {
                     $( this ).dialog( "close" );
                 }
             }
-        ]
-    });
-    //Click
-    $( "#dialog-msg" ).click(function( event ) {
-        $( "#pop" ).dialog( "open" );
-        event.preventDefault();
+        ]*/
     });
 
+
+    //goes into sever detail info page
+
+    $("div[name='citybox']").click(function(){
+
+        //clean pop box content
+        $("#server_green").html();
+        $("#server_warning").html();
+        $("#server_gray").html();
+
+        //Get City Code fromid
+        var cityCode= $(this).attr("id");
+        $( "#pop" ).dialog( "open" );
+        event.preventDefault();
+        var greenHtml = "<img title='美国服务器1' src='image/server-green.gif' width='70' height=70'/><img title='美国服务器1' src='image/server-green.gif' width='70' height=70'/>";
+        var warningHtml = "<img title='美国服务器1' src='image/server-warning.gif' width='70' height=70'/>";
+        var grayHtml = "<img title='美国服务器1' src='image/server-gray.gif' width='70' height=70'/>";
+
+        $("#server_green").html(greenHtml);
+        $("#server_warning").html(warningHtml);
+        $("#server_gray").html(grayHtml);
+    });
 });//end of function
 //
 
